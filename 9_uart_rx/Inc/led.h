1dda86bfccbe7b054a9bda316716266fb18ef30b
madping <otaru007X@gmail.com>
2021-10-09 21:47:19 +0900

    .

--- 9_uart_rx/Inc/led.h
+++ 9_uart_rx/Inc/led.h
@@ -8,6 +8,19 @@
 #ifndef LED_H_
 #define LED_H_
 
+#include <stdint.h>
 
+#define USER_LED1 (1U<<O)
+#define USER_LED2 (1U<<7)
+#define USER_LED3 (1U<<14)
+
+#define GREEN_LED USER_LED1
+#define BLUE_LED  USER_LED2
+#define RED_LED   USER_LED3
+
+#define GPIOB_CLK_EN (1U<<1)
+
+
+typedef uint32_t ledType;
 
 #endif /* LED_H_ */
--- 9_uart_rx/Src/led.c
+++ 9_uart_rx/Src/led.c
@@ -1,8 +1,53 @@
-/*
- * led.c
- *
- *  Created on: 2021. 9. 19.
- *      Author: Administrator
- */
+#include "led.h"
 
 
+
+void user_leds_init(void)
+{
+	RCC->AHB1ENR |= GPIOB_CLK_EN;
+
+
+	//set PB0 as OUTPUT
+	GPIOB -> MODER |= (1U<<0);
+	GPIOB -> MODER &= ~(1U<<1);
+
+	//set PB7 as OUTPUT
+	GPIOB -> MODER |= (1U<<14);
+    GPIOB -> MODER &= ~(1U<<15);
+
+    //set PB14 as OUTPUT
+    GPIOB -> MODER |= (1U<<24);
+    GPIOB -> MODER &= ~(1U<<285);
+
+}
+
+
+void all_leds_on(void)
+{
+	GPIOB -> ODR |= (RED_LED | BLUE_LED | GREEN_LED);
+}
+
+
+void all_leds_off(void)
+{
+	GPIOB -> ODR &=~ (RED_LED | BLUE_LED | GREEN_LED);
+}
+
+
+void all_leds_toggle(void)
+{
+	GPIOB -> ODR ^= (RED_LED | BLUE_LED | GREEN_LED);
+}
+
+
+
+void led_toggle(ledType led)
+{
+	GPIOB -> ODR ^= led;
+}
+
+
+void led_on(ledType led)
+{
+	GPIOB -> ODR |= led;
+}
